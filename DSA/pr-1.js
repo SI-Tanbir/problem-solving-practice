6c860275c28e14a98b52bb328cf73c56317f5aca
SI-Tanbir <si017490@gmail.com>
2024-11-21 18:23:56 +0600

    adding problem 1 which is about check if the number is upercase or lowercase

---
+++ DSA/pr-1.js
@@ -0,0 +1,16 @@
+// suppose you have a character ... you have to determine if this charater is uppercase or lowercase
+
+
+
+let ch ='A';
+
+if (ch >= 'a' && ch<= 'z'){
+
+    console.log("it is lowercase ")
+
+}
+else{
+
+    console.log("uppercase")
+    
+}
